598ba685568dad1fde82b4a065e5a8b2a59c5020
Guglielmo <guglielmo.celata@gmail.com>
2014-02-28 18:32:03 +0100

    documentation added in comments for main views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,15 +7,21 @@ __author__ = 'guglielmo'
 
 class ApiRootView(APIView):
     """
-    Lista di tutte le legislature
+    This is the main access point to the set of API at Openpolis
+
+    * **politici** - italian politicians and their charges (from `http://politici.openpolis.it`)
+    * **parlamento** - italian parliament acts and votations (16th and 17th legislature)
+    * **maps** - locations, and locations classifications, with contextual data and geometric information
+    * **territori** - an old, simpler version of the locations API (deprecated)
+
     """
     def get(self, request, **kwargs):
         format = kwargs.get('format', None)
         data = SortedDict([
-            ('maps', reverse('maps:api-root', request=request, format=format)),
             ('politici', reverse('politici:api-root', request=request, format=format)),
             ('parlamento', reverse('parlamento:legislatura-list', request=request, format=format)),
-            ('territori', reverse('territori:api-root', request=request, format=format)),            
+            ('maps', reverse('maps:api-root', request=request, format=format)),
+            ('territori', reverse('territori:api-root', request=request, format=format)),
         ])
         return Response(data)
 
